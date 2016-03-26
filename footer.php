0c4e359f3b0f1a6298f6f12d7f2f49eeecc71231
Tom Nehil <tom.nehil@gmail.com>
2016-03-26 16:37:04 -0500

    Adds archive and cat dropdowns to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,23 @@
 		<div class="footer">
+			<div class="footer-nav">
+				Browse archives by
+
+				Month:
+				<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+				  <option value=""><?php echo esc_attr( __( 'Select Month' ) ); ?></option>
+				  <?php wp_get_archives( 'type=monthly&format=option&show_post_count=1' ); ?>
+				</select>
+
+				Category:
+				<?php $args = array(
+					show_option_none	 => 'Select Category',
+					'orderby'            => 'NAME',
+					'show_count'         => 1,
+				); ?>
+				<?php wp_dropdown_categories( $args ); ?>
+
+			</div>
+
 			<p>
 				<?php _e('&#169; Martha Garc&eacute;s and Tom Nehil. All Rights Reserved.'); ?>
 				<?php printf(__('%1$s and %2$s.', 'marthaandtom'), '<a href="' . get_bloginfo('rss2_url') . '">' . __('Entries (RSS)', 'marthaandtom') . '</a>', '<a href="' . get_bloginfo('comments_rss2_url') . '">' . __('Comments (RSS)', 'marthaandtom') . '</a>'); ?>
@@ -9,10 +28,10 @@
 			 * tag of your theme, or you will break many plugins, which
 			 * generally use this hook to reference JavaScript files.
 			 */
-		
+
 			wp_footer();
 		?>
-		
+
 		<!-- Google Analytics-->
 		<script type="text/javascript">
 			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
@@ -23,6 +42,14 @@
 			var pageTracker = _gat._getTracker("UA-7422160-1");
 			pageTracker._trackPageview();
 			} catch(err) {}
+
+			var dropdown = document.getElementById("cat");
+			function onCatChange() {
+				if ( dropdown.options[dropdown.selectedIndex].value > 0 ) {
+					location.href = "<?php echo esc_url( home_url( '/' ) ); ?>?cat="+dropdown.options[dropdown.selectedIndex].value;
+				}
+			}
+		dropdown.onchange = onCatChange;
 		</script>
 	</body>
 </html>
