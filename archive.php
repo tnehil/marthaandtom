3204b069bf265187c0f8cfb4c8dba151420c51cc
Tom Nehil <tom.nehil@gmail.com>
2016-03-26 16:01:02 -0500

    Adds thumbnails to archive pages

--- archive.php
+++ archive.php
@@ -23,6 +23,18 @@
 		<?php while (have_posts()) : the_post(); ?>
 
 			<div class="post" id="post-<?php the_ID(); ?>">
+
+				<?php
+					if ( has_post_thumbnail() ) {?>
+						<div class="archive-post-thumbnail">
+							<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(the_title_attribute('echo=0')); ?>">
+							<?php set_post_thumbnail_size(250,166,true);
+					    	the_post_thumbnail();?>
+							</a>
+						</div>
+					<?php }
+				?>
+
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h2>
 
 				<h3 class="postmeta"><?php if (!is_author()) : _e('By','marthaandtom') ;?> <a href="<?php echo bloginfo('url') .'/'. the_author() ?>"><?php the_author() ?></a> // <?php endif;?> <span class="grey"><?php the_time(__('j F Y', 'marthaandtom')) ?> in: <?php echo get_the_category_list(', '); ?></span> &nbsp; <span class="orange"><?php comments_popup_link(__('No comments', 'marthaandtom'), __('1 comment', 'marthaandtom'), __('% comments', 'marthaandtom'), '','' ); ?></span></h3>
