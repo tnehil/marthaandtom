4763242b4f6aec8d3c83a30520174c6a8c0f5f14
Tom Nehil <tom.nehil@gmail.com>
2016-04-03 16:39:50 -0500

    Removes extra space under post meta on search results while preserving it on single and index page. Adds space-under class which is probably kind of lame

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			<div class="post" id="post-<?php the_ID(); ?>">
 				<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h1>
 
-				<h3 class="postmeta"><?php _e('By','marthaandtom');?> <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a> // <span class="grey"><?php printf( __('Posted %1$s in: %2$s','marthaandtom'), get_the_time(__('j F, Y', 'marthaandtom')), get_the_category_list(', ')) ;?></span> &nbsp; <span class="orange"><?php just_comments_popup_link(__('No comments', 'marthaandtom'), __('1 comment', 'marthaandtom'), __('% comments', 'marthaandtom'), '','' ); ?></span></h3>	
+				<h3 class="postmeta space-under"><?php _e('By','marthaandtom');?> <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a> // <span class="grey"><?php printf( __('Posted %1$s in: %2$s','marthaandtom'), get_the_time(__('j F, Y', 'marthaandtom')), get_the_category_list(', ')) ;?></span> &nbsp; <span class="orange"><?php just_comments_popup_link(__('No comments', 'marthaandtom'), __('1 comment', 'marthaandtom'), __('% comments', 'marthaandtom'), '','' ); ?></span></h3>	
 
 				<div class="entry">
 					<?php the_content(__('Keep reading &raquo;', 'marthaandtom')); ?>
