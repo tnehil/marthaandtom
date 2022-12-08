efdc63dbff2c0f7c23592b4c64ade1acc6a0b016
Tom Nehil <tom@toms-mbp.lan>
2022-12-07 21:26:04 -0600

    Goodbye GA

--- footer.php
+++ footer.php
@@ -31,18 +31,7 @@
 
 			wp_footer();
 		?>
-
-		<!-- Google Analytics-->
-		<script type="text/javascript">
-			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-			</script>
-			<script type="text/javascript">
-			try {
-			var pageTracker = _gat._getTracker("UA-7422160-1");
-			pageTracker._trackPageview();
-			} catch(err) {}
-
+		<script>
 			var dropdown = document.getElementById("cat");
 			function onCatChange() {
 				if ( dropdown.options[dropdown.selectedIndex].value > 0 ) {
